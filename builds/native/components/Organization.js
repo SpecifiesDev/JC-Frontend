70e00238d4d43aa38546decb9bfa79a6ad4a9239
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2020-03-15 05:02:44 -0400

    Move Organization from DirectoryScreen.js to separate component

---
+++ builds/native/components/Organization.js
@@ -0,0 +1,129 @@
+import React from 'react';
+
+import {Text, View, StyleSheet} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import {RectButton} from 'react-native-gesture-handler';
+import Modal from 'react-native-modal';
+
+export class Organization extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = { rowColor: {}, modalVisible: false, website: null,  websiteStyle: { color: 'blue' }, websitePress: null };
+  
+      if(this.props.position % 2 == 0) {
+        this.state.rowColor = {backgroundColor: '#e8e8e8'}
+      } else {
+        this.state.rowColor = {backgroundColor: '#fdfdfd'}
+      }
+  
+      if(this.props.website == "N/A") { 
+        this.state.websiteStyle = {color: 'black'}; this.state.websitePress = () => {};
+        this.state.website = "N/A"
+      }
+      else if(this.props.website.length > 16) { 
+        this.state.website = "Link"
+        this.state.websitePress = () => { WebBrowser.openBrowserAsync(this.props.website)}
+      }
+      else {
+        this.state.website = this.props.website;
+        this.state.websitePress = () => { WebBrowser.openBrowserAsync(this.props.website)}
+      }
+  
+    
+    }
+  
+    setModalVisible() {
+      this.setState({ rowColor: this.state.rowColor, modalVisible: true, website: this.state.website,  websiteStyle: this.state.websiteStyle, websitePress: this.state.websitePress });
+    }
+  
+    render() {
+      return(
+  
+        <React.Fragment>
+            <Modal
+                isVisible = {this.state.modalVisible}
+                onBackdropPress = {() => { this.setState({rowColor: this.state.rowColor, modalVisible: false, website: this.state.website, websiteStyle: this.state.websiteStyle })}}
+                animationIn = "bounceIn"
+                backDropOpacity = {1}
+                backdropTransitionInTiming = {1000}
+                animationInTiming = {1000}
+                onSwipeComplete = {() => {this.setState({ rowColor: this.state.rowColor, modalVisible: false, website: this.state.website,  websiteStyle: this.state.websiteStyle, websitePress: this.state.websitePress })}}
+                swipeDirection = "right"
+              >
+              <View style = {styles.modalContent}>
+                <Text style = {styles.modalTitle}>{this.props.name}</Text>
+                <Text style = {styles.modalBase}>{this.props.description}</Text>
+  
+                <Text style = {styles.modalBase}>
+                  <Text style = {styles.bold}>Website: </Text>
+                  <Text onPress = {this.state.websitePress} style = {this.state.websiteStyle}>{this.state.website}</Text>
+                </Text>
+                <Text style = {styles.modalBase}>
+                  <Text style = {styles.bold}>Phone: </Text>
+                  <Text>{this.props.phone}</Text>
+                </Text>
+                <Text style = {styles.modalBase}>
+                  <Text style = {styles.bold}>Address: </Text>
+                  <Text>{this.props.address}</Text>
+                </Text>
+              </View>
+            </Modal>
+            
+  
+            <RectButton style={[styles.option, this.props.isLastOption && styles.lastOption, this.state.rowColor]} onPress={() => {this.setModalVisible();}}>
+              <View style = {{flexDirection: 'row'}}>
+                <View style={styles.optionIconContainer}>
+                  <Ionicons name={this.props.icon} size={22} color="rgba(0,0,0,0.35)" />
+                </View>
+                <View style={styles.optionTextContainer}>
+                  <Text style={styles.optionText}>{this.props.name}</Text>
+                </View>
+              </View>
+  
+            </RectButton>
+        </React.Fragment>
+        
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+      optionIconContainer: {
+        marginRight: 12,
+      },
+      option: {
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderBottomWidth: 0,
+        borderColor: '#ededed',
+      },
+      lastOption: {
+        borderBottomWidth: StyleSheet.hairlineWidth,
+      },
+      optionText: {
+        fontSize: 15,
+        alignSelf: 'flex-start',
+        marginTop: 1,
+      },
+      modalTitle: {
+        fontSize: 19
+      },
+      modalBase: {
+        paddingTop: 10,
+        fontSize: 16
+      },
+      bold: {
+        fontWeight: 'bold'
+      },
+      modalContent: {
+        backgroundColor: 'white',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)'
+      }
+  })
--- builds/native/screens/DirectoryScreen.js
+++ builds/native/screens/DirectoryScreen.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
- import * as WebBrowser from 'expo-web-browser';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet,  ActivityIndicator } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { SplashScreen } from 'expo';
-import Modal from 'react-native-modal';
+import { Organization } from '../components/Organization';
 
 const axios = require('axios');
 
@@ -67,88 +65,7 @@ export default function DirectoryScreen(props) {
 
 }
 
-class Organization extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { rowColor: {}, modalVisible: false, website: null,  websiteStyle: { color: 'blue' }, websitePress: null };
-
-    if(this.props.position % 2 == 0) {
-      this.state.rowColor = {backgroundColor: '#e8e8e8'}
-    } else {
-      this.state.rowColor = {backgroundColor: '#fdfdfd'}
-    }
-
-    if(this.props.website == "N/A") { 
-      this.state.websiteStyle = {color: 'black'}; this.state.websitePress = () => {};
-      this.state.website = "N/A"
-    }
-    else if(this.props.website.length > 16) { 
-      this.state.website = "Link"
-      this.state.websitePress = () => { WebBrowser.openBrowserAsync(this.props.website)}
-    }
-    else {
-      this.state.website = this.props.website;
-      this.state.websitePress = () => { WebBrowser.openBrowserAsync(this.props.website)}
-    }
-
-  
-  }
-
-  setModalVisible() {
-    this.setState({ rowColor: this.state.rowColor, modalVisible: true, website: this.state.website,  websiteStyle: this.state.websiteStyle, websitePress: this.state.websitePress });
-  }
-
-  render() {
-    return(
-
-      <React.Fragment>
-          <Modal
-              isVisible = {this.state.modalVisible}
-              onBackdropPress = {() => { this.setState({rowColor: this.state.rowColor, modalVisible: false, website: this.state.website, websiteStyle: this.state.websiteStyle })}}
-              animationIn = "bounceIn"
-              backDropOpacity = {1}
-              backdropTransitionInTiming = {1000}
-              animationInTiming = {1000}
-              onSwipeComplete = {() => {this.setState({ rowColor: this.state.rowColor, modalVisible: false, website: this.state.website,  websiteStyle: this.state.websiteStyle, websitePress: this.state.websitePress })}}
-              swipeDirection = "right"
-            >
-            <View style = {styles.modalContent}>
-              <Text style = {styles.modalTitle}>{this.props.name}</Text>
-              <Text style = {styles.modalBase}>{this.props.description}</Text>
-
-              <Text style = {styles.modalBase}>
-                <Text style = {styles.bold}>Website: </Text>
-                <Text onPress = {this.state.websitePress} style = {this.state.websiteStyle}>{this.state.website}</Text>
-              </Text>
-              <Text style = {styles.modalBase}>
-                <Text style = {styles.bold}>Phone: </Text>
-                <Text>{this.props.phone}</Text>
-              </Text>
-              <Text style = {styles.modalBase}>
-                <Text style = {styles.bold}>Address: </Text>
-                <Text>{this.props.address}</Text>
-              </Text>
-            </View>
-          </Modal>
-          
-
-          <RectButton style={[styles.option, this.props.isLastOption && styles.lastOption, this.state.rowColor]} onPress={() => {this.setModalVisible();}}>
-            <View style = {{flexDirection: 'row'}}>
-              <View style={styles.optionIconContainer}>
-                <Ionicons name={this.props.icon} size={22} color="rgba(0,0,0,0.35)" />
-              </View>
-              <View style={styles.optionTextContainer}>
-                <Text style={styles.optionText}>{this.props.name}</Text>
-              </View>
-            </View>
-
-          </RectButton>
-      </React.Fragment>
-      
-    );
-  }
-}
 
 function getIconByTag(tagString) {
 
@@ -196,41 +113,5 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
-  },
-  modalTitle: {
-    fontSize: 19
-  },
-  modalBase: {
-    paddingTop: 10,
-    fontSize: 16
-  },
-  bold: {
-    fontWeight: 'bold'
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4,
-    borderColor: 'rgba(0, 0, 0, 0.1)'
   }
 });
