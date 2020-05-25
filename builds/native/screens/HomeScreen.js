af7a3ffc7f9f25b6fb5f34dca6962da77186aacb
Carson21 <carsonkeavney@gmail.com>
2020-05-24 20:44:07 -0400

    Remove buttons and align spons. vert. on Home page

--- builds/native/screens/HomeScreen.js
+++ builds/native/screens/HomeScreen.js
@@ -1,37 +1,25 @@
 import * as React from 'react';
-import { StyleSheet, Text, ScrollView, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen() {
   return (
-      <ScrollView contentContainerStyle={styles.contentContainer}>
-        <View style={styles.head}>
-          <Text style={styles.header}>Welcome to a new way of serving your community</Text>
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View style={styles.head}>
+        <Text style={styles.header}>Welcome to a new way of serving your community</Text>
 
-          <Text style={styles.bodyText}>
-            Jackson Connect is dedicated to creating ways for you to serve your community and providing easy accessable help for people in your community.
-          </Text>
-        </View>
-        
-        <View style={styles.buttons}>
-          <Text style={{fontWeight: '600', fontSize: 22}}>Do you need help?</Text>
-          <TouchableOpacity style={styles.directory} onPress={() => navigation.navigate("Directory")}>
-            <Text style={{color: '#fff', fontSize: 18}}>Check out our Directory page!</Text>
-          </TouchableOpacity>
-          
-          <Text style={{fontWeight: '600', fontSize: 22}}>Want to help?</Text>
-          <TouchableOpacity style={styles.posts} onPress={() => navigation.navigate("Posts")}>
-            <Text style={{color: '#fff', fontSize: 18}}>Check out our Posts page!</Text>
-          </TouchableOpacity>
-        </View>
+        <Text style={styles.bodyText}>
+          Jackson Connect is dedicated to creating ways for you to serve your community and providing easy accessable help for people in your community.
+        </Text>
+      </View>
 
-        <Text style={{fontSize: 23, fontWeight: '600', textAlign: 'center'}}>Sponsors:</Text>
+      <Text style={{ fontSize: 23, fontWeight: '600', textAlign: 'center' }}>Sponsors:</Text>
 
-        <View style={styles.sponsors}>
-          <Image style={{width: 130, height: 100, resizeMode: 'stretch'}} source={require('../assets/images/northeastgahealth.jpg')} />
-          <Image style={{width: 200, height: 60, resizeMode: 'stretch'}} source={require('../assets/images/piedmontathens.png')} />
-        </View>
-        
-      </ScrollView>
+      <View style={styles.sponsors}>
+        <Image style={{ width: 130, height: 100 }} resizeMode={"contain"} source={require('../assets/images/northeastgahealth.jpg')} />
+        <Image style={{ width: 200, height: 60, marginTop: 30 }} resizeMode={"contain"} source={require('../assets/images/piedmontathens.png')} />
+      </View>
+
+    </ScrollView>
   );
 }
 
@@ -62,32 +50,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 
-  buttons: {
-    marginBottom: 50,
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-
-  directory: {
-    padding: 15,
-    borderRadius: 5,
-    backgroundColor: '#17a2b8',
-    marginTop: 10,
-    marginBottom: 20
-  },
-
-  posts: {
-    padding: 15,
-    borderRadius: 5,
-    backgroundColor: '#28a745',
-    marginTop: 10,
-    marginBottom: 20
-  },
-
   sponsors: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginVertical: 15
+    marginVertical: 15,
+    flex: 1
   }
 });
